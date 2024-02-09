0eb9e9ab82918335370159e067c4e05bcdb8f907
aparody <aparody@unomaha.edu>
2024-02-09 14:22:12 -0600

    added pagination

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const config = {
-    images: {
-        remotePatterns: [{hostname: "images.unsplash.com"}],
-    },
-    experimental: {
-      serverActions: true,
-    },
-  }
-   
-  module.exports = config
+  images: {
+    remotePatterns: [
+      { hostname: "images.unsplash.com" },
+      { hostname: "lh3.googleusercontent.com" },
+    ],
+  },
+  experimental: {
+    serverActions: true,
+  },
+};
+
+module.exports = config;
